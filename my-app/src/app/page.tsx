fd7835cd00422bd56195799bf7ffb80cce3fcbbe
JRheeTU <93626877+JoshuaRheee@users.noreply.github.com>
2025-01-24 13:46:41 -0500

    Update page.tsx

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -22,7 +22,7 @@ export default function Home() {
 
   return (
     <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', backgroundColor: '#000000' }}>
-      <div style={{ padding: '20px', maxWidth: '400px', textAlign: 'center', borderRadius: '8px', background: '#fff' }}>
+      <div style={{ padding: '20px', maxWidth: '400px', textAlign: 'center', borderRadius: '8px', background: '#101010' }}>
         <div style={{
           width: '200px',
           height: '200px',
